31d8de0bf30547f9fd2df8ceaf8e9a47ab7c3107
polina <polindrom@memos.tk>
2022-01-04 14:31:33 +0300

    second commit

---
+++ test/ru/java/reverse/ReverseTest.java
@@ -0,0 +1,15 @@
+package ru.java.reverse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReverseTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals("olleH!dlroW", Reverse.reverse("Hello World!"));
+	}
+
+}
